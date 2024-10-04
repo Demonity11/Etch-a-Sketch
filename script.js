52c5b3cce12117b47946935ca3f7f8a88a13ce0f
Marcelo Esau <xxdm_11@outlook.com>
2024-10-04 13:33:55 -0300

    Resize grid option added.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function createSquares() {
+function createSquares(event) {
     const container = document.querySelector(".container");
+    let numberOfSquares = 256;
+    
+    if (event) {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.remove();
+        });
 
-    for (let i = 1; i <= 256; i++) {
+        let squaresX = parseInt(prompt("Type the number of the horizontal squares: "));
+        let squaresY = parseInt(prompt("Type the number of the vertical squares: "));
+        numberOfSquares = squaresX * squaresY;
+
+        container.style.width = `${squaresX * 44}px`;
+        container.style.height = `${squaresY * 44}px`;
+    }
+
+    for (let i = 1; i <= numberOfSquares; i++) {
         const square = document.createElement("div");
         square.setAttribute("class", "square");
         container.appendChild(square);
@@ -9,3 +24,8 @@ function createSquares() {
 }
 
 createSquares();
+
+const createNewGridBtn = document.querySelector("#newGrid");
+createNewGridBtn.addEventListener("click", (e) => {
+    createSquares(e);
+});
