f0668e1c5f8fca7f2b8d9ec107ed58c9b01adfc6
Marcelo Esau <xxdm_11@outlook.com>
2024-10-05 06:40:52 -0300

    Added a hoverable rgb grid.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createSquares(event) {
     const container = document.querySelector(".container");
-    let numberOfSquares = 256;
+    let numberOfSquares = 16;
     
     if (event) {
         const squares = document.querySelectorAll(".square");
@@ -8,28 +8,29 @@ function createSquares(event) {
             square.remove();
         });
 
-        let squaresX = 0;
-        let squaresY = 0;
-
         do {
-            squaresX = parseInt(prompt("Type the number of the horizontal squares: "));
-            squaresY = parseInt(prompt("Type the number of the vertical squares: "));
+            numberOfSquares = parseInt(prompt("Type the number of squares per side (max 100): "));
 
-            if (squaresX > 100 || squaresY > 100) alert("You cannot asign values over 100.");
+            if (numberOfSquares > 100) alert("You cannot asign values over 100.");
 
-        } while (squaresX > 100 || squaresY > 100)
-
-        container.style.width = `${squaresX * 44}px`;
-        container.style.height = `${squaresY * 44}px`;
-
-        numberOfSquares = squaresX * squaresY;
+        } while (numberOfSquares > 100)
     }
 
-    for (let i = 1; i <= numberOfSquares; i++) {
+    for (let i = 1; i <= numberOfSquares**2; i++) {
         const square = document.createElement("div");
         square.setAttribute("class", "square");
+
+        square.style.height = `${704 / numberOfSquares -2}px`;
+        square.style.width = `${704 / numberOfSquares -2}px`;
         container.appendChild(square);
     }
+
+    container.addEventListener("mouseover", (e) => {
+        const random_RGB_values = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+        let target = e.target;
+        
+        target.style.backgroundColor = `rgb(${random_RGB_values.join(",")})`;
+    });
 }
 
 createSquares();
